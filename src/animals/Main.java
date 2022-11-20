8391afee8b676ea62437a6ea35e7bef906d11051
Sergey Khaykov <ssserg3000@gmail.com>
2022-11-20 16:56:41 +0200

    last commit

--- src/animals/Main.java
+++ src/animals/Main.java
@@ -1,15 +1,17 @@
+package animals;
+
 import animals.Animals;
 
 public class Main {
     public static void main(String[] args) {
         Animals animal1 = Animals.RABBIT;
-        System.out.println(animal1.toString());
+        System.out.println(animal1);
         Animals animal2 = Animals.FOX;
-        System.out.println(animal2.toString());
+        System.out.println(animal2);
         Animals animal3 = Animals.HORSE;
-        System.out.println(animal3.toString());
+        System.out.println(animal3);
         Animals animal4 = Animals.MONKEY;
-        System.out.println(animal4.toString());
+        System.out.println(animal4);
 
     }
 }
--- src/vehicle/Main.java
+++ src/vehicle/Main.java
@@ -3,10 +3,10 @@ package vehicle;
 public class Main {
     public static void main(String[] args) {
         Vehicle car1 = Vehicle.AUDI;
-        System.out.println(car1.toString());
+        System.out.println(car1);
         Vehicle car2 = Vehicle.BMW;
-        System.out.println(car2.toString());
+        System.out.println(car2);
         Vehicle car3 = Vehicle.MERCEDES;
-        System.out.println(car3.toString());
+        System.out.println(car3);
     }
 }
